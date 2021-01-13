c2ae32c7f754366a8ad86ef7fdeb3c6c2eb775f2
Sam Vaughan <sam.vaughan@sydney.edu.au>
2021-01-13 14:28:53 +1100

    typo in filename in example script

--- Ayoan_example_script.py
+++ Ayoan_example_script.py
@@ -7,7 +7,7 @@ A pipeline for Hector observations
 from hop import pipeline 
 
 
-HP = pipeline.HectorPipe(config_filename="ConfigFiles/Ayoan_example_config_Cluster2")
+HP = pipeline.HectorPipe(config_filename="ConfigFiles/Ayoan_example_config_Cluster2.yaml")
 
 HP.allocate_hexabundles_for_single_tile(tile_number=0)
 
