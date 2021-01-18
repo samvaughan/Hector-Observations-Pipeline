f295a1742394d7d383440b8a9ea00e4ddab14c63
Ayoan <asad9520@uni.sydney.edu.au>
2021-01-18 11:25:07 +1100

    Added commenting

--- hop/hexabundle_allocation/general_operations/geometry_intersections/segment_with_segment.py
+++ hop/hexabundle_allocation/general_operations/geometry_intersections/segment_with_segment.py
@@ -1,9 +1,13 @@
+
+# calculating intersection point between a vertical segment and slope segment
 def intersection_slope_segment_and_vertical_segment(vertical_segment, slope_segment):
 
+    # calculating intersection point of vertical and slope segments
     line = slope_segment.create_line_equation()
     intersection_point_y = line.calculate_y_from_x(vertical_segment.start_point[0])
     intersection_point = [vertical_segment.start_point[0],intersection_point_y]
 
+    # checking whether intersection point lies on both vertical and slope segments
     if vertical_segment.point_is_on_segment(intersection_point):
 
         if slope_segment.point_is_on_segment(intersection_point):
@@ -17,10 +21,12 @@ def intersection_slope_segment_and_vertical_segment(vertical_segment, slope_segm
 
         return None
 
+# calculating intersection between two segments
 def segments_intersection(segment1,segment2):
 
     intersection_point =[]
 
+    # intersection calculation for either one vertical or both vertical segments
     if segment1.vertical():
 
         if segment2.vertical():
@@ -33,10 +39,12 @@ def segments_intersection(segment1,segment2):
 
             intersection_point = intersection_slope_segment_and_vertical_segment(segment1,segment2)
 
+    # intersection calculation for one vertical and one slope segment
     elif segment2.vertical():
 
         intersection_point = intersection_slope_segment_and_vertical_segment(segment2,segment1)
 
+    # intersection calculation for both slope segments by creating line equations
     else:
         line1 = segment1.create_line_equation()
         line2 = segment2.create_line_equation()
