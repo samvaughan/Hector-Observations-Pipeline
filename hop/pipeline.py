1c308dc391a573d57d4e7914265a629e827bd25d
Sam Vaughan <sam.vaughan@sydney.edu.au>
2021-01-21 15:15:31 +1100

    deleted DC check

--- hop/pipeline.py
+++ hop/pipeline.py
@@ -68,8 +68,8 @@ class HectorPipe:
 
         # Get the location of the distortion correction executable and the R code
         self.DistortionCorrection_binary_location = Path(__file__).parent / Path("distortion_correction/HectorTranslationSoftware/Code/HectorConfigUtil")
-        if not self.DistortionCorrection_binary_location.exists():
-            raise NameError("The Distortion Correction binary seems to not exist")
+        # if not self.DistortionCorrection_binary_location.exists():
+        #     raise NameError("The Distortion Correction binary seems to not exist")
 
         self.TdF_distortion_file_location = Path(__file__).parent / Path("distortion_correction/HectorTranslationSoftware/Code/tdFdistortion0.sds")  
         if not self.TdF_distortion_file_location.exists():
