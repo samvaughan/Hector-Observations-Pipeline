18ec4ae8fa7ccc5510b69adc99ed2c755713f068
Ayoan <asad9520@uni.sydney.edu.au>
2021-04-06 14:45:16 +1000

    adjustments to run new G15 batch

--- Ayoan_example_script.py
+++ Ayoan_example_script.py
@@ -9,7 +9,7 @@ from hop import pipeline
 
 HP = pipeline.HectorPipe(config_filename="ConfigFiles/Ayoan_example_config_Cluster2.yaml")
 
-HP.allocate_hexabundles_for_single_tile(tile_number=21,plot=True)
-
+for i in range(1):
+    HP.allocate_hexabundles_for_single_tile(tile_number=i,plot=True)
 
 
