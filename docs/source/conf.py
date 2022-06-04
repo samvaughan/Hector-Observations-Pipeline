148a770fe767b1e8f50e057701697229fbfa323c
Sam Vaughan <sam.vaughan@sydney.edu.au>
2022-06-04 22:03:46 +1000

    Added favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,9 @@ exclude_patterns = []
 html_theme = 'bootstrap'
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
+html_logo = '_static/Hector-Galaxy-Survey-favicon.png'
+html_favicon = '_static/Hector-Galaxy-Survey-favicon.png'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
